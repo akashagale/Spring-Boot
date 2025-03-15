167918f0b65b8ef1195f479feacd04d353622aac
akashagale <aagale014@gmail.com>
2025-03-15 18:53:11 +0530

    Update

--- 16.1_SpringBoot_Project_Blog/src/main/java/com/myapp/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.myapp.entity.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long>{
-		
-}
