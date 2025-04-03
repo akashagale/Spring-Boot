a1224bdd15e11bc329df071eada6da585a842405
akashagale <aagale014@gmail.com>
2025-04-03 23:03:39 +0530

    Update

--- ecommerce/ecommerce/src/main/java/com/product/app/repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.product.app.repository;
-
-import com.product.app.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-}
--- ecommerce/ecommerce/src/main/java/com/product/app/service/PublicService.java
+++
@@ -1,9 +0,0 @@
-package com.product.app.service;
-
-import com.product.app.entity.Product;
-
-import java.util.List;
-
-public interface PublicService {
-    public List<Product> getProducts();
-}
